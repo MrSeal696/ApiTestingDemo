dda083ea90e59185da7a4aa8282f359462306238
BAZA <BAZA@DESKTOP-01JLC19>
2026-02-17 20:45:00 +0300

    Добавьте файлы проекта.

---
+++ ApiTestingDemo/Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTestingDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> users = new List<User>()
+        {
+            new User { Id = 1, Name = "Ivan", Age = 25 },
+            new User { Id = 2, Name = "Maria", Age = 30 }
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            user.Id = users.Max(x => x.Id) + 1;
+            users.Add(user);
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, User updatedUser)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+                return NotFound();
+
+            user.Name = updatedUser.Name;
+            user.Age = updatedUser.Age;
+
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+                return NotFound();
+
+            users.Remove(user);
+            return Ok();
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ ApiTests/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using RestSharp;
+
+namespace ApiTests
+{
+    public class Tests
+    {
+        private RestClient client;
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new RestClient("https://localhost:7216");
+        }
+
+        [Test]
+        public void GetUsers_ShouldReturn200()
+        {
+            var request = new RestRequest("/api/users", Method.Get);
+
+            var response = client.Execute(request);
+
+            Assert.That((int)response.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void CreateUser_ShouldReturn200()
+        {
+            var request = new RestRequest("/api/users", Method.Post);
+
+            request.AddJsonBody(new
+            {
+                name = "TestUser",
+                age = 20
+            });
+
+            var response = client.Execute(request);
+
+            Assert.That((int)response.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void UpdateUser_ShouldReturn200()
+        {
+            var request = new RestRequest("/api/users/1", Method.Put);
+
+            request.AddJsonBody(new
+            {
+                name = "Updated",
+                age = 40
+            });
+
+            var response = client.Execute(request);
+
+            Assert.That((int)response.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void DeleteUser_ShouldReturn200()
+        {
+            var request = new RestRequest("/api/users/1", Method.Delete);
+
+            var response = client.Execute(request);
+
+            Assert.That((int)response.StatusCode, Is.EqualTo(200));
+        }
+    }
+}
